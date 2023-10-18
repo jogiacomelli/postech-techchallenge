feb6236ca9d469ee6a9f92315e901def61d884c6
Joseane Giacomelli <jogiacomelli.cc@gmail.com>
2023-10-18 17:39:03 -0300

    First Commit

---
+++ src/main/java/br/com/postech/techchallenge/TechchallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.postech.techchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechchallengeApplication.class, args);
+	}
+
+}
